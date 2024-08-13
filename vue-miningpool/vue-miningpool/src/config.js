734f1052ecdd1f1d87b8a741c182d8c232196a5b
MiningCryptoLive <celldr69@gmail.com>
2024-08-13 09:13:59 -0400

    Update config.js

--- vue-miningpool/vue-miningpool/src/config.js
+++ vue-miningpool/vue-miningpool/src/config.js
@@ -1,14 +1,14 @@
 export default {
-  pool_name: "pool-node",
-  name: "Mix",
-  symbol: "MIX",
-  algo: "Ethash",
+  pool_name: "pool-mcl",
+  name: "Bch",
+  symbol: "BCH",
+  algo: "sha256d",
   scheme: "SOLO",
   fee: 1,
   valid_address: 42,
-  min_payout: 0.1,
-  block_reward: 5,
-  block_time: 15,
+  min_payout: 3.0,
+  block_reward: 3.125,
+  block_time: 10m,
   explorer: {
     height: "https://explorer.ballzcoin.org/block/",
     uncle: "https://explorer.ballzcoin.org/uncle/",
@@ -16,29 +16,21 @@ export default {
     tx: "https://explorer.ballzcoin.org/tx/"
   },
   pool: {
-    wallet: '0x6a6ee2d7d06aa9b47b25957ec1e1d8124837ffec',
-    host: 'eth-solo.pool-node.com',
+    wallet: '143DHQFdXz9wfwvLXUDBVp7zigLAgRBU5j',
+    host: 'bch.solopool.pro',
     port: {
-      '0.5': 11101,
-      '1': 11102,
-      '1.5': 11103,
+      '100000': 11101,
+      '500000': 11102,
+      '1000000': 11103,
     }
   },
   api: {
-    url: "http://192.168.100.100:8080/api/",
+    url: "http://bch.solopool.pro/api/",
     time: 15000
   },
   pools: [{
-      name: "Ethereum Classic",
-      url: "etc.pool-node.com"
-    },
-    {
-      name: "Ethereum",
-      url: "eth.pool-node.com"
-    },
-    {
-      name: "Ballzcoin",
-      url: "http://test.pool-node.com"
+      name: "Bitcoin-Cash",
+      url: "bch.solopool.pro"
     }
   ]
 };
